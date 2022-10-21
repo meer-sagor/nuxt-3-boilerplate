32f2b0e9cb30ccf8e81b85dab49e1388ab15c2d2
meer-sagor <meersagor.contact@gmail.com>
2022-10-21 20:40:30 +0600

    added vueuse package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,28 @@
-import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
-  head: {
-    title: process.env.npm_package_name,
-    meta: [
-      { charset: 'utf-8' },
-      {
-        name: 'viewport',
-        hid: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },      
-      {
-        property: 'apple-mobile-web-app-capable',
-        hid: 'apple-mobile-web-app-capable',
-        content: 'yes',
-      },
-      {
-        property: 'apple-touch-fullscreen',
-        hid: 'apple-touch-fullscreen',
-        content: 'yes',
-      },
-    ],
-    link: [],
+export default {
+  app: {
+    head: {
+      title: 'nuxt-3-boilerplate',
+      meta: [
+        { charset: 'utf-8' },
+        {
+          name: 'viewport',
+          hid: 'viewport',
+          content: 'width=device-width, initial-scale=1',
+        },
+        {
+          property: 'apple-mobile-web-app-capable',
+          hid: 'apple-mobile-web-app-capable',
+          content: 'yes',
+        },
+        {
+          property: 'apple-touch-fullscreen',
+          hid: 'apple-touch-fullscreen',
+          content: 'yes',
+        },
+      ],
+      link: [],
+    },
   },
   modules: [
     '@nuxtjs/tailwindcss',
@@ -38,5 +38,6 @@ export default defineNuxtConfig({
         ],
       },
     ],
+    '@vueuse/nuxt',
   ],
-})
+}
